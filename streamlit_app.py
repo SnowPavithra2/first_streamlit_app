2e68a5328bd48685fd027ea6a75baafe36d0085e
SnowPavithra2 <135967101+SnowPavithra2@users.noreply.github.com>
2023-06-08 16:37:19 +0530

    Update streamlit_app.py
    Testing the text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.write('Breakfast Menu')
+streamlit.write('#Breakfast Menu
+                  Omega 3 & Blueberry Oatmeal
+                  Kale,Spinach & Rocket Smoothie
+                  Hard-Boiled Free-Range Egg')
 streamlit.write('Omega 3 & Blueberry Oatmeal')
 streamlit.write('Kale,Spinach & Rocket Smoothie')             
 streamlit.write('Hard-Boiled Free-Range Egg')    
