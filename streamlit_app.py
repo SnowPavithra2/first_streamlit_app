686ad38f34db526e6570e4cf09fb916a668f4227
SnowPavithra2 <135967101+SnowPavithra2@users.noreply.github.com>
2023-06-08 16:38:50 +0530

    Update streamlit_app.py
    Testing the position of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.write('#Breakfast Menu
+streamlit.write("""
+                  #Breakfast Menu
                   Omega 3 & Blueberry Oatmeal
                   Kale,Spinach & Rocket Smoothie
-                  Hard-Boiled Free-Range Egg')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale,Spinach & Rocket Smoothie')             
-streamlit.write('Hard-Boiled Free-Range Egg')    
+                  Hard-Boiled Free-Range Egg
+                """)
